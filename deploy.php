07495b45f85b234867972d78e994dbdeb69b171c
branchard <benoit.branchard@gmail.com>
2017-11-28 10:36:12 +0100

    init commit

---
+++ deploy.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * This file has been generated automatically.
+ * Please change the configuration for correct use deploy.
+ */
+
+require 'recipe/symfony.php';
+
+// Set configurations
+set('repository', 'git@domain.com:username/repository.git');
+set('shared_files', ['app/config/parameters.yml']);
+set('shared_dirs', ['app/logs']);
+set('writable_dirs', ['app/cache', 'app/logs']);
+
+// Configure servers
+server('production', 'prod.domain.com')
+    ->user('username')
+    ->password()
+    ->env('deploy_path', '/var/www/prod.domain.com');
+
+server('beta', 'beta.domain.com')
+    ->user('username')
+    ->password()
+    ->env('deploy_path', '/var/www/beta.domain.com');
+
+server('test', '54.37.15.4')
+    ->user('root')
+    ->password('IcbZ3gl6')
+    ->env('deploy_path', '/var/www/my_symfony');
+
+/**
+ * Restart php-fpm on success deploy.
+ */
+task('php-fpm:restart', function () {
+    // Attention: The user must have rights for restart service
+    // Attention: the command "sudo /bin/systemctl restart php-fpm.service" used only on CentOS system
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo /bin/systemctl restart php-fpm.service');
+})->desc('Restart PHP-FPM service');
+
+after('success', 'php-fpm:restart');
+
+
+/**
+ * Attention: This command is only for for example. Please follow your own migrate strategy.
+ * Attention: Commented by default.
+ * Migrate database before symlink new release.
+ */
+
+// before('deploy:symlink', 'database:migrate');
